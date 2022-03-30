9c5b8d32cf5fa20e67f866e3620ce3825dd6ccbf
Raphael Kalinowski <raphael@myminicom.com>
2022-03-30 16:11:55 +0100

    added devise yml FR translation and i18n gem

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Internationalization i18n -v 7.0.0
+# the locales which have to be loaded via
+config.i18n.available_locales = ['fr-FR', :fr]
