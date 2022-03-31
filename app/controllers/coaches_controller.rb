6e9a5693b543de70bebbe3f561fcc5cc43d61acf
Raphael Kalinowski <raphael@myminicom.com>
2022-03-31 20:58:05 +0100

    add new user

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,7 +1,16 @@
 class CoachesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show, :edit]
+  skip_before_action :authenticate_user!, only: [:edit, :search, :verify_authenticity_token]
 
   def edit
     @coach = current_coach
+  end
+
+  def search
+    @user = User.find(params[:id])
+    if(@user)
+      redirect_to new_training_path(user_id: @user.id)
+    else
+      render 'root'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :users, only: [:show, :edit]
-  resources :coaches, only: :edit
+  resources :coaches, only: [:edit]
+  post 'search', to: 'coaches#search'
+  get 'about', to: 'pages#about'
   resources :trainings, only: [:new, :create, :show, :destroy, :edit, :update]
 end
