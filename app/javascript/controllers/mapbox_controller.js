4ee166a5c3d0cf576df911d3b49384a5534c81e4
Raphael Kalinowski <raphael@myminicom.com>
2022-04-01 21:03:08 +0100

    quick fix geocoder

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -18,3 +18,22 @@ export default class extends Controller {
     })
   }
 }
+
+connect() {
+  this.#addMarkersToMap()
+  this.#fitMapToMarkers()
+}
+
+#addMarkersToMap() {
+  this.markersValue.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(this.map)
+  });
+}
+
+#fitMapToMarkers() {
+  const bounds = new mapboxgl.LngLatBounds()
+  this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+  this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+}
