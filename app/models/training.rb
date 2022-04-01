25fd2567cb42d78c5ee1045ace847d23457a7ea2
Raphael Kalinowski <raphael@myminicom.com>
2022-04-01 19:23:30 +0100

    added .env and gitignore it

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,5 +1,7 @@
 class Training < ApplicationRecord
   belongs_to :coach
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   # has_many :movements, dependent: :destroy
 end
