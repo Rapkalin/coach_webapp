3cd73c6f6cce703e7f0fdabfc1cc49f1f8c4b932
Raphael Kalinowski <raphael@myminicom.com>
2022-08-19 14:45:55 +0200

    Get trainings for a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,18 @@
-class UsersController < ApplicationController
+class UsersController < HomesController
   skip_before_action :authenticate_user!, only: [:show, :edit]
 
+  def edit
+    @user = current_user
+  end
+
   def show
-    @passed_trainings = []
-    @coming_trainings = []
     if current_coach
-      # @trainings = training.select('user_id').from('trainings').group(:user_id).where(coach_id: current_coach.id)
-      # @trainings = training.select('user_id').from('trainings').where(coach_id: current_coach.id)
       @user = User.find(params[:id])
-      @trainings = @user.trainings.where(coach_id: current_coach.id)
+      @passed_trainings = []
+      @coming_trainings = []
+
+      @trainings = Training.select('*').where(user_id: @user.id, coach_id: current_coach.id)
       @trainings.each do |training|
-        if training.date < Time.now
-          @passed_trainings.push(training)
-          @passed_trainings
-        elsif training.date >= Time.now
-          @coming_trainings.push(training)
-          @coming_trainings
-        end
-      end
-    elsif current_user
-      @user = current_user
-      @trainings = @user.trainings
-      @trainings.each_with_index do |training, index|
         if training.date < Time.now
           @passed_trainings.push(training)
           @passed_trainings
@@ -33,8 +24,5 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = current_user
-  end
 
 end
